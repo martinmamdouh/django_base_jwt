a5cf3acbd134f1cd8815568b96698838233f54dd
marthabib <marthabib@etisalat.ae>
2019-06-16 12:18:03 +0400

    adding requirements.txt

--- client/apn_api.py
+++ client/apn_api.py
@@ -1,7 +1,9 @@
 import requests
-headers={'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwidXNlcl9pZCI6MSwiZXhwIjoxNTYwMDgxMTQyLCJqdGkiOiI3ZTFmY2E1MTVjMmY0MmVkYTczZjExZDRjNTYzNDY3MSJ9.dVb8H-OgyvKf3UH3DOIzvcyZWAbYI5DntasCMT3cVRc'}
-r = requests.get('http://127.0.0.1:8000/apis/apn',headers=headers)#, auth=('user', 'pass'))
+headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NjA2NzMxOTksImp0aSI6IjU4MjkxYzU0Y2I4YzRlYmQ4NTUxYTVhMGU5ZWEwNDEzIiwidXNlcl9pZCI6MSwidG9rZW5fdHlwZSI6ImFjY2VzcyIsIm5hbWUiOiJtYXJ0aW4ifQ.w_k8gTstyJWQs8Izt4zcHQGSsk5JkTlYjanwwU56pMc'}
+# , auth=('user', 'pass'))
+r = requests.get('http://127.0.0.1:8000/apis/apn', headers=headers)
 print(r.headers['content-type'])
 print(r.encoding)
 print(r.status_code)
-print(r.json())
+# print(r.json())
+# print(r.text)
--- client/generate_token.py
+++ client/generate_token.py
@@ -1,3 +1,4 @@
 import requests
-r=requests.post('http://127.0.0.1:8000/api-auth/token/',data={"username": "martin", "password": ""})
+r = requests.post('http://127.0.0.1:8000/api-auth/token/',
+                  data={"username": "martin", "password": "EWQ#ewq3"})
 print(r.text)
